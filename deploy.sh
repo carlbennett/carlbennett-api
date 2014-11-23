f00532e441d27d36625f00c1f82866976f2f01b9
Carl Bennett <Jailout2000@users.noreply.github.com>
2014-11-22 19:56:42 -0600

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+printf "== Deploying to api.carlbennett.me =============================================\n"
+
+printf "==== Syncing source code =======================================================\n"
+rsync -Oprtvz --exclude='.git/' --exclude='.gitignore' --exclude='settings.sample.json' . root@carlbennett.me:/home/nginx/carlbennett-api
+printf "==== Changing user and group ownership =========================================\n"
+ssh root@carlbennett.me chown nginx:webusers -Rv /home/nginx/carlbennett-api | grep -v "^ownership of .* retained as .*$"
+
+printf "== Deployments done ============================================================\n"
