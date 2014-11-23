9bbe0ae643bbb8e76520d7d5f0c77ca8d15700d9
Carl Bennett <Jailout2000@users.noreply.github.com>
2014-11-22 19:59:06 -0600

    Add a couple more exclusions to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 printf "== Deploying to api.carlbennett.me =============================================\n"
 
 printf "==== Syncing source code =======================================================\n"
-rsync -Oprtvz --exclude='.git/' --exclude='.gitignore' --exclude='settings.sample.json' . root@carlbennett.me:/home/nginx/carlbennett-api
+rsync -Oprtvz --exclude='.git/' --exclude='.gitignore' --exclude='deploy.sh' --exclude 'README.md' --exclude='settings.sample.json' . root@carlbennett.me:/home/nginx/carlbennett-api
 printf "==== Changing user and group ownership =========================================\n"
 ssh root@carlbennett.me chown nginx:webusers -Rv /home/nginx/carlbennett-api | grep -v "^ownership of .* retained as .*$"
 
