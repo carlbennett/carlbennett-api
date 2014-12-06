57e9105d17f03caebc78432ef5b602d781944bfa
Carl Bennett <Jailout2000@users.noreply.github.com>
2014-12-05 23:28:14 -0600

    Miscellaneous deploy script improvements.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-printf "== Deploying to api.carlbennett.me =============================================\n"
+DEPLOY_SOURCE="/home/nginx/carlbennett-api-local/"
+DEPLOY_TARGET="/home/nginx/carlbennett-api/"
+
+printf "== Deploying api.carlbennett.me ================================================\n"
+
+printf "Source: $DEPLOY_SOURCE\n"
+printf "Target: $DEPLOY_TARGET\n"
 
 printf "==== Syncing source code =======================================================\n"
-rsync -Oprtvz --delete --exclude='.git/' --exclude='.gitignore' --exclude='deploy.sh' --exclude 'README.md' --exclude='settings.sample.json' . root@carlbennett.me:/home/nginx/carlbennett-api
+rsync -Oprtvz --delete --exclude='.git/' --exclude='.gitignore' --exclude='deploy.sh' --exclude 'README.md' --exclude='settings.sample.json' "$DEPLOY_SOURCE" "carl@carlbennett.me:$DEPLOY_TARGET"
+
 printf "==== Changing user and group ownership =========================================\n"
-ssh root@carlbennett.me chown nginx:webusers -Rv /home/nginx/carlbennett-api | grep -v "^ownership of .* retained as .*$"
+ssh carl@carlbennett.me sudo chown nginx:webusers -Rv /home/nginx/carlbennett-api | grep -v "^ownership of .* retained as .*$"
 
 printf "== Deployments done ============================================================\n"
