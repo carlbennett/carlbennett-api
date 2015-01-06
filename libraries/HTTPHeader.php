2bd6f9e66ca61b9cbc4a91f4378e63e6f455e150
Carl Bennett <Jailout2000@users.noreply.github.com>
2015-01-06 04:50:36 -0600

    Rename HTTPHeader to Pair, create HTTPHeader subclass.

--- libraries/HTTPHeader.php
+++ libraries/HTTPHeader.php
@@ -2,32 +2,16 @@
 
 namespace CarlBennett\API\Libraries;
 
-class HTTPHeader implements \Serializable {
+use CarlBennett\API\Libraries\Pair;
 
-  public function __construct($name, $value) {
-    $this->name = $name;
-    $this->value = $value;
-  }
+class HTTPHeader extends Pair {
 
   public function getName() {
-    return $this->name;
-  }
-
-  public function getValue() {
-    return $this->value;
+    return $this->getKey();
   }
 
   public function __tostring() {
-    return $this->name . ": " . $value . "\n";
-  }
-
-  public function serialize() {
-    return serialize([$this->name, $this->value]);
-  }
-
-  public function unserialize($data) {
-    $this->name = $data[0];
-    $this->value = $data[1];
+    return $this->key . ": " . $this->value . "\n";
   }
 
 }
