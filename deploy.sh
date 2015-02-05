3ae025c8639d0dd2eb7326c7e97da1adf7a6e745
Carl Bennett <Jailout2000@users.noreply.github.com>
2015-02-05 11:40:17 -0600

    Improve rsync file exclusion.

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ printf "Source: $DEPLOY_SOURCE\n"
 printf "Target: $DEPLOY_TARGET\n"
 
 printf "==== Syncing source code =======================================================\n"
-rsync --rsync-path='sudo rsync' -Oprtvz --delete --exclude='.git/' --exclude='.gitignore' --exclude='deploy.sh' --exclude 'README.md' --exclude='settings.sample.json' "$DEPLOY_SOURCE" "carl@carlbennett.me:$DEPLOY_TARGET"
+rsync --rsync-path='sudo rsync' -Oprtvz --delete --exclude-from='rsync-exclude.txt' "$DEPLOY_SOURCE" "carl@carlbennett.me:$DEPLOY_TARGET"
 
 printf "==== Changing user and group ownership =========================================\n"
 ssh carl@carlbennett.me sudo chown nginx:webusers -Rv /home/nginx/carlbennett-api | grep -v "^ownership of .* retained as .*$"
