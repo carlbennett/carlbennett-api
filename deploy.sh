151b22a01c9e720d7e88c0a48aa57c6bc8596441
Carl Bennett <Jailout2000@users.noreply.github.com>
2015-02-16 03:25:54 +0000

    New deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,45 @@
-#!/bin/sh
+#!/bin/bash
+printf "===============================================================================\n"
 
-DEPLOY_SOURCE="/home/nginx/carlbennett-api-local/"
-DEPLOY_TARGET="/home/nginx/carlbennett-api/"
+printf "== Configuring environment ====================================================\n"
+DEPLOY_SOURCE_PATH=$(git rev-parse --show-toplevel)
+DEPLOY_TARGET_HOST="carlbennett.me"
+DEPLOY_TARGET_USER="carl"
+DEPLOY_TARGET_PATH="/home/nginx/carlbennett-www"
 
-printf "== Deploying api.carlbennett.me ================================================\n"
+printf "DEPLOY_SOURCE_PATH = ${DEPLOY_SOURCE_PATH}\n"
+printf "DEPLOY_TARGET_HOST = ${DEPLOY_TARGET_HOST}\n"
+printf "DEPLOY_TARGET_USER = ${DEPLOY_TARGET_USER}\n"
+printf "DEPLOY_TARGET_PATH = ${DEPLOY_TARGET_PATH}\n"
 
-printf "Source: $DEPLOY_SOURCE\n"
-printf "Target: $DEPLOY_TARGET\n"
+printf "== Sanity check ===============================================================\n"
+[ "$DEPLOY_SOURCE_PATH" == "" ] && \
+  printf "Error: DEPLOY_SOURCE_PATH is empty.\n" && exit 1
+[ "$DEPLOY_TARGET_HOST" == "" ] && \
+  printf "Error: DEPLOY_TARGET_HOST is empty.\n" && exit 1
+[ "$DEPLOY_TARGET_USER" == "" ] && \
+  printf "Error: DEPLOY_TARGET_USER is empty.\n" && exit 1
+[ "$DEPLOY_TARGET_PATH" == "" ] && \
+  printf "Error: DEPLOY_TARGET_PATH is empty.\n" && exit 1
 
-printf "==== Syncing source code =======================================================\n"
-rsync --rsync-path='sudo rsync' -Oprtvz --delete --exclude-from='rsync-exclude.txt' "$DEPLOY_SOURCE" "carl@carlbennett.me:$DEPLOY_TARGET"
+printf "== Deploying to target ========================================================\n"
+CUR_DIR=`pwd` && cd "$DEPLOY_SOURCE_PATH"
+CODE="$?" && [ "$CODE" -ne 0 ] && exit "$CODE"
 
-printf "==== Changing user and group ownership =========================================\n"
-ssh carl@carlbennett.me sudo chown nginx:webusers -Rv /home/nginx/carlbennett-api | grep -v "^ownership of .* retained as .*$"
+rsync --rsync-path="sudo rsync" -Oprtvz --delete \
+  --exclude-from="rsync-exclude.txt" \
+  "$DEPLOY_SOURCE_PATH" \
+  "${DEPLOY_TARGET_USER}@${DEPLOY_TARGET_HOST}:${DEPLOY_TARGET_PATH}"
+CODE="$?" && [ "$CODE" -ne 0 ] && exit "$CODE"
 
-printf "== Deployments done ============================================================\n"
+cd "$CUR_DIR"
+CODE="$?" && [ "$CODE" -ne 0 ] && exit "$CODE"
+
+printf "== Changing permissions on target =============================================\n"
+ssh "${DEPLOY_TARGET_USER}@${DEPLOY_TARGET_HOST}" \
+  sudo chown nginx:www-data -Rv "$DEPLOY_TARGET_PATH" | \
+  grep -v "^ownership of .* retained as .*$"
+CODE="$?" && [ "$CODE" -ne 0 ] && exit "$CODE"
+
+printf "===============================================================================\n"
+exit 0
