a49089bc2aa2e381d6f8e3ee3b5f0996d42731f6
Carl Bennett <Jailout2000@users.noreply.github.com>
2015-03-16 14:49:30 -0500

    Fix duplicate error code.

--- libraries/Exceptions/MethodNotAllowedException.php
+++ libraries/Exceptions/MethodNotAllowedException.php
@@ -5,7 +5,7 @@ namespace CarlBennett\API\Libraries\Exceptions;
 class MethodNotAllowedException extends APIException {
 
   public function __construct($allowed_methods, $prev_ex = null) {
-    parent::__construct("HTTP method not allowed", 3, $prev_ex);
+    parent::__construct("HTTP method not allowed", 7, $prev_ex);
     $this->httpResponseCode = 405;
     $this->setHTTPResponseHeader("Allow", implode(",", $allowed_methods));
   }
