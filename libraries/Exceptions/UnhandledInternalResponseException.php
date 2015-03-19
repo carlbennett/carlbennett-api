e06d5e3410f1f6c25bcd691a9c0ac94d3801e6bd
Carl Bennett <Jailout2000@users.noreply.github.com>
2015-03-19 18:54:12 +0000

    Fix params of UnhandledInternalResponseException.

--- libraries/Exceptions/UnhandledInternalResponseException.php
+++ libraries/Exceptions/UnhandledInternalResponseException.php
@@ -4,7 +4,7 @@ namespace CarlBennett\API\Libraries\Exceptions;
 
 class UnhandledInternalResponseException extends APIException {
 
-  public function __construct($allowed_methods, $prev_ex = null) {
+  public function __construct($prev_ex = null) {
     parent::__construct("Unhandled internal response", 8, $prev_ex);
     $this->httpResponseCode = 500;
   }
