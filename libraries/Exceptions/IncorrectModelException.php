4ea661de5d880da0ebf12b4fc5883ac43499188c
Carl Bennett <carlbennett@users.noreply.github.com>
2015-09-08 19:56:11 +0000

    Remove unused string in IncorrectModelException

--- libraries/Exceptions/IncorrectModelException.php
+++ libraries/Exceptions/IncorrectModelException.php
@@ -7,7 +7,7 @@ use \Exception;
 
 class IncorrectModelException extends APIException {
 
-  public function __construct($extensionName, Exception $prev_ex = null) {
+  public function __construct(Exception $prev_ex = null) {
     parent::__construct("Incorrect model provided to view", 3, $prev_ex);
   }
 
