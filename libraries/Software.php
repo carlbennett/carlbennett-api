d48ca6c0d6ca1b8da2f275d698d9a627efc52b90
Carl Bennett <carlbennett@users.noreply.github.com>
2015-09-26 05:01:23 +0000

    Add software products and versions to config file

--- libraries/Software.php
+++ libraries/Software.php
@@ -26,24 +26,42 @@ class Software {
     $result->product = (isset($data->product) ? $data->product : null);
     $result->version = (isset($data->version) ? $data->version : null);
 
-    switch (strtolower($result->product)) {
-      case null: case "": {
+    if (is_null($result->product) || $result->product == "") {
+      return $result;
+    }
+
+    $product_found = false;
+    foreach (Common::$config->Software->products as $product) {
+      if (strtolower($product->name) == strtolower($result->product)) {
+        $product_found = true;
+        $new_version   = null;
+        $http_code     = 200;
+        foreach ($product->versions as $version) {
+          if (version_compare(
+            $result->version, $version->version, "<"
+          )) {
+            $new_version = $version;
+          }
+        }
+        if (!is_null($new_version)) {
+          $result->status->code    = self::UPDATE_STATUS_VERSION_OLD;
+          $result->status->message = "Version is old";
+          $result->update          = new StdClass();
+          $result->update->url     = $version->url;
+          $result->update->version = $version->version;
+        } else {
+          $result->status->code    = self::UPDATE_STATUS_VERSION_CURRENT;
+          $result->status->message = "Version is current";
+        }
         break;
-      }
-      case "bnrbot": {
-        // Until this is implemented, return unknown version.
-        $http_code               = 200;
-        $result->status->code    = self::UPDATE_STATUS_VERSION_UNKNOWN;
-        $result->status->message = "Version unknown";
-        break;
-      }
-      default: {
-        $http_code               = 404;
-        $result->status->code    = self::UPDATE_STATUS_PRODUCT_UNKNOWN;
-        $result->status->message = "Unknown product";
       }
     }
-    
+    if (!$product_found) {
+      $http_code               = 404;
+      $result->status->code    = self::UPDATE_STATUS_PRODUCT_UNKNOWN;
+      $result->status->message = "Unknown product";
+    }
+
     return $result;
   }
 
