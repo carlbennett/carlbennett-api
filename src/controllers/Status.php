6f6df647aaa19e0698e99e7830ebf18f78fa24cc
Carl Bennett <carl@carlbennett.me>
2018-10-19 17:42:50 -0500

    Add database and memcache healthcheck

--- src/controllers/Status.php
+++ src/controllers/Status.php
@@ -4,8 +4,11 @@ namespace CarlBennett\API\Controllers;
 
 use \CarlBennett\API\Libraries\VersionInfo;
 use \CarlBennett\API\Models\Status as StatusModel;
+use \CarlBennett\MVC\Libraries\Cache;
 use \CarlBennett\MVC\Libraries\Common;
 use \CarlBennett\MVC\Libraries\Controller;
+use \CarlBennett\MVC\Libraries\Database;
+use \CarlBennett\MVC\Libraries\DatabaseDriver;
 use \CarlBennett\MVC\Libraries\GeoIP;
 use \CarlBennett\MVC\Libraries\Router;
 use \CarlBennett\MVC\Libraries\View;
@@ -30,11 +33,19 @@ class Status extends Controller {
   }
 
   protected function getStatus( StatusModel &$model ) {
+    if ( !isset( Common::$database ) ) {
+      Common::$database = DatabaseDriver::getDatabaseObject();
+    }
+
     $status = new StdClass();
+
+    $healthcheck           = new StdClass();
+    $healthcheck->database = ( Common::$database instanceof Database );
+    $healthcheck->memcache = ( Common::$cache instanceof Cache );
 
     $utc = new DateTimeZone( 'Etc/UTC' );
 
-    $status->healthcheck       = true;
+    $status->healthcheck       = $healthcheck;
     $status->is_browser        = Common::isBrowser(getenv( 'HTTP_USER_AGENT' ));
     $status->remote_address    = getenv( 'REMOTE_ADDR' );
     $status->remote_geoinfo    = GeoIP::get( $status->remote_address );
@@ -43,6 +54,13 @@ class Status extends Controller {
     $status->version_info      = VersionInfo::$version;
 
     $model->status = $status;
+
+    foreach ( $healthcheck as $key => $val ) {
+      if ( is_bool( $val ) && !$val ) {
+        // let the controller know that we're unhealthy.
+        return false;
+      }
+    }
 
     return true;
   }
