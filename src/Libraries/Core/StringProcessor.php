c453b9176890f8dffcf4ba5f737b596d28a5c49b
Carl Bennett <carl@carlbennett.me>
2025-09-27 05:26:11 -0500

    Add additional functions to StringProcessor

--- src/Libraries/Core/StringProcessor.php
+++ src/Libraries/Core/StringProcessor.php
@@ -4,6 +4,49 @@ namespace CarlBennett\API\Libraries\Core;
 
 class StringProcessor
 {
+    /**
+     * Matches a pattern to a fuzzy subject, e.g. 'api.*' should match 'api.enabled'
+     *
+     * @param string $pattern The pattern that the subject should match, e.g. 'api.*'
+     * @param string $subject The subject that is being compared, e.g. 'api.enabled'
+     * @return boolean Whether the string matches.
+     */
+    public static function fuzzyMatch(string $pattern, string $subject): bool
+    {
+        $pattern = \preg_replace_callback('/([^*])/', 'preg_quote', $pattern);
+        $pattern = \str_replace('*', '.*', $pattern);
+        return (bool) \preg_match('/^' . $pattern . '$/i', $subject);
+    }
+
+    /**
+     * Checks whether the current User Agent is a GUI web browser known by a common string, e.g. Firefox.
+     *
+     * @param string $user_agent User Agent to check, or null to check HTTP_USER_AGENT environment variable.
+     * @return boolean Whether the client is using a GUI web browser or not.
+    */
+    public static function isBrowser(?string $user_agent = null): bool
+    {
+        return (1 === \preg_match(
+            '#(?:AppleWebKit|Chrome|Edge|Firefox|Mozilla|MSIE|Opera|Safari|Trident|WebKit)#i',
+            $user_agent ?? \getenv('HTTP_USER_AGENT') ?? ''
+        ));
+    }
+
+    public static function sanitizeForUrl(array|string $haystack, bool $lowercase = true): array|string
+    {
+        $result = \preg_replace('/[^\da-z]+/im', '-', $haystack);
+        if (\is_string($result))
+        {
+            $result = \trim($result, '-');
+        }
+        else
+        {
+            \array_walk($result, '\trim', '-');
+        }
+        if ($lowercase) $result = \strtolower($result);
+        return $result;
+    }
+
     public static function stripExcessLines($buffer): string
     {
         return \preg_replace("/\n\n+/", "\n\n", $buffer);
